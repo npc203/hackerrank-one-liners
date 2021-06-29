3729ba90a98be87807b6430b0890ace69578a561
npc203 <npc203@users.noreply.github.com>
2021-06-29 14:28:32 +0530

    [Auto] Formatting

--- set remove discord pop.py
+++ set remove discord pop.py
@@ -1,2 +1,1 @@
-(lambda _: (lambda s: (lambda _ : print(sum(s)))([(lambda a: getattr(s,a[0])(*map(int,a[1:])))(input().split()) for i in range(int(input()))])
-)(set(map(int, input().split()))) )(input())
+(lambda _: (lambda s: (lambda _ : print(sum(s)))([(lambda a: getattr(s,a[0])(*map(int,a[1:])))(input().split()) for i in range(int(input()))]))(set(map(int, input().split()))) )(input())
